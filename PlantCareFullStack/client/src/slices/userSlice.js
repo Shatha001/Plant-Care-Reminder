d65cd7fb2efa50ac2738984e960570c995f97173
Shatha001 <16j20127@utas.edu.om>
2025-12-11 16:09:32 +0400

    Update userSlice.js

--- PlantCareFullStack/client/src/slices/userSlice.js
+++ PlantCareFullStack/client/src/slices/userSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_URL = "https://plant-care-reminder.onrender.com";
+const API_URL = "http://localhost:7500";
 
 // Register new user
 export const registerThunk = createAsyncThunk(
@@ -99,5 +99,6 @@ const userSlice = createSlice({
 
 export const { logout, reset } = userSlice.actions;
 export default userSlice.reducer;
+
 
 
